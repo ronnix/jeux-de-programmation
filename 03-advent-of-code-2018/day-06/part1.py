91267d2227d87117beefe842b4969089d4fd6509
Ronan Amicel <ronan.amicel@gmail.com>
2018-12-06 21:59:41 +0100

    Day 6, part 1: Add function to compute Manhattan distance

--- 03-advent-of-code-2018/day-06/part1.py
+++ 03-advent-of-code-2018/day-06/part1.py
@@ -11,6 +11,21 @@ def read_input(stream):
     return (tuple(map(int, line.strip().split(", "))) for line in stream)
 
 
+def test_manhattan_distance():
+    assert manhattan_distance((0, 0), (0, 0)) == 0
+    assert manhattan_distance((0, 0), (1, 1)) == 2
+    assert manhattan_distance((1, 1), (0, 0)) == 2
+    assert manhattan_distance((5, 0), (1, 1)) == 5
+    assert manhattan_distance((5, 0), (5, 5)) == 5
+    assert manhattan_distance((5, 0), (8, 3)) == 6
+
+
+def manhattan_distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
 def main():
     print(list(read_input(sys.stdin)))
 
