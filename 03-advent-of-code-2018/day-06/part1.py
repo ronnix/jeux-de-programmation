498e1fff044968ae59dfa59dac00fe2d7f25fb6e
Ronan Amicel <ronan.amicel@gmail.com>
2018-12-06 23:04:50 +0100

    Day 6, part 1: Add function to check if a location has an infinite area

--- 03-advent-of-code-2018/day-06/part1.py
+++ 03-advent-of-code-2018/day-06/part1.py
@@ -59,6 +59,47 @@ def closest_locations(locations, coordinates):
     }
 
 
+def test_has_infinite_area():
+    locations = [
+        (1, 1),  # A
+        (1, 6),  # B
+        (8, 3),  # C
+        (3, 4),  # D
+        (5, 5),  # E
+        (8, 9),  # F
+    ]
+    assert has_infinite_area(locations, (1, 1))  # A
+    assert has_infinite_area(locations, (1, 6))  # B
+    assert has_infinite_area(locations, (8, 3))  # C
+    assert not has_infinite_area(locations, (3, 4))  # D
+    assert not has_infinite_area(locations, (5, 5))  # E
+    assert has_infinite_area(locations, (8, 9))  # F
+
+
+def has_infinite_area(locations, location):
+    """
+    If we go in any one of the 4 directions, and for all the coordinates
+    we find until we reach the edge of the grid, the location is the closest
+    one, then it has in infinite area.
+    """
+    x_min, y_min = 0, 0
+    x_max, y_max = grid_size(locations)
+    x_loc, y_loc = location
+
+    up = ((x_loc, y) for y in range(y_min, max(y_min, y_loc)))
+    down = ((x_loc, y) for y in range(min(y_max, y_loc + 1), y_max + 1))
+    left = ((x, y_loc) for x in range(x_min, max(x_min, x_loc)))
+    right = ((x, y_loc) for x in range(min(x_max, x_loc + 1), x_max + 1))
+
+    return any(
+        all(
+            closest_locations(locations, point) == {location}
+            for point in axis
+        )
+        for axis in (up, down, left, right)
+    )
+
+
 def main():
     print(list(read_input(sys.stdin)))
 
