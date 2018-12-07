30e743b8f30d41fe60e47a12afed21a4f4b47671
Ronan Amicel <ronan.amicel@gmail.com>
2018-12-07 23:21:14 +0100

    Day 7, part 2: Add function to compute required time to process steps

--- 03-advent-of-code-2018/day-07/day-07.py
+++ 03-advent-of-code-2018/day-07/day-07.py
@@ -66,9 +66,53 @@ def step_time(step, base_time=60):
     return base_time + ord(step) - ord("A") + 1
 
 
+def test_required_time():
+    dependency_graph = read_input(TEST_INPUT.splitlines())
+    print(dependency_graph)
+    assert required_time(dependency_graph, workers=2, base_time=0) == 15
+
+
+def required_time(dependency_graph, workers, base_time):
+    time = -1
+    completed = ""
+    available_workers = workers
+    schedule = defaultdict(list)
+    remaining = dependency_graph.copy()
+
+    while remaining or schedule:
+
+        # Move time forward
+        time += 1
+
+        # Have any started steps been completed?
+        for step in schedule[time]:
+            completed += step
+            available_workers += 1
+        del schedule[time]
+
+        # Can we start a new step?
+        while available_workers:
+            ready = [
+                step
+                for step, required in remaining.items()
+                if all(dep in completed for dep in required)
+            ]
+            if ready:
+                step = min(ready)
+                print(f"Starting step {step} at time {time}")
+                available_workers -= 1
+                del remaining[step]
+                schedule[time + step_time(step, base_time)].append(step)
+            else:
+                break
+
+    return time
+
+
 def main():
     dependency_graph = read_input(sys.stdin)
     print(walk_graph(dependency_graph))
+    print(required_time(dependency_graph, workers=5, base_time=60))
 
 
 if __name__ == "__main__":
