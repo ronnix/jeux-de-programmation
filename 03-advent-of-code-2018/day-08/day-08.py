b76da1236ef496eeb74311e6557bbce9a9a3c251
Ronan Amicel <ronan.amicel@gmail.com>
2018-12-08 22:48:40 +0100

    Day 8, part 1: add building of tree from sequence of numbers

--- 03-advent-of-code-2018/day-08/day-08.py
+++ 03-advent-of-code-2018/day-08/day-08.py
@@ -30,8 +30,63 @@ def parse_input(text):
     return [int(s) for s in text.split()]
 
 
+def test_build_empty_tree():
+    assert Node.from_numbers([0, 0]) == Node()
+
+
+def test_build_tree_with_metadata():
+    assert Node.from_numbers([0, 1, 42]) == Node(metadata=[42])
+
+
+def test_build_tree_with_child():
+    assert Node.from_numbers([1, 0, 0, 0]) == Node(children=[Node()])
+
+
+def test_build_tree():
+    assert Node.from_numbers(parse_input(TEST_INPUT)) == Node(
+        children=[
+            Node(metadata=[10, 11, 12]),
+            Node(children=[Node(metadata=[99])], metadata=[2]),
+        ],
+        metadata=[1, 1, 2],
+    )
+
+
+class Node:
+    def __init__(self, children=None, metadata=None):
+        self.children = children if children is not None else []
+        self.metadata = metadata if metadata is not None else []
+
+    def __repr__(self):
+        return f"Node(children={self.children!r}, metadata={self.metadata!r}"
+
+    def __eq__(self, other):
+        return self.children == other.children and self.metadata == other.metadata
+
+    @classmethod
+    def from_numbers(cls, numbers):
+        node, length = cls._from_numbers(numbers)
+        assert length == len(numbers)
+        return node
+
+    @classmethod
+    def _from_numbers(cls, numbers):
+        node = cls()
+        nb_children = numbers[0]
+        nb_metadata = numbers[1]
+        index = 2
+        for _ in range(nb_children):
+            child, length = cls._from_numbers(numbers[index:])
+            node.children.append(child)
+            index += length
+        node.metadata = numbers[index : index + nb_metadata]
+        return node, index + nb_metadata
+
+
 def main():
-    print(parse_input(sys.stdin.read()))
+    numbers = parse_input(sys.stdin.read())
+    tree = Node.from_numbers(numbers)
+    print(tree)
 
 
 if __name__ == "__main__":
