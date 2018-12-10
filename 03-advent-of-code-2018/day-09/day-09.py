e5a19ac98c3b4ecae017a34ed6e54804289cdf96
Ronan Amicel <ronan.amicel@gmail.com>
2018-12-10 12:05:08 +0100

    Day 9, part 1: add function to add a new marble (simple case)

--- 03-advent-of-code-2018/day-09/day-09.py
+++ 03-advent-of-code-2018/day-09/day-09.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 
+import pytest
+
 
 def test_parse():
     assert parse("426 players; last marble is worth 72058 points") == (426, 72058)
@@ -9,6 +11,34 @@ def test_parse():
 def parse(text):
     words = text.split()
     return int(words[0]), int(words[6])
+
+
+@pytest.mark.parametrize("marbles, current, number, new_marbles, new_current", [
+    ([0], 0, 1, [0, 1], 1),
+    ([0, 1], 1, 2, [0, 2, 1], 1),
+    ([0, 2, 1], 1, 3, [0, 2, 1, 3], 3),
+    (
+        [0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15],
+        11,
+        22,
+        [0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15],
+        13,
+    ),
+
+])
+def test_add_marble(marbles, current, number, new_marbles, new_current):
+    assert add_marble(marbles, current, number) == (new_marbles, new_current)
+
+
+def add_marble(marbles, current, number):
+    new_marbles = marbles.copy()
+    new_current = (current + 2) % len(marbles)
+    if new_current == 0:
+        new_current = len(marbles)
+        new_marbles.append(number)
+    else:
+        new_marbles.insert(new_current, number)
+    return new_marbles, new_current
 
 
 def main():
