7b9f86398a3be6fd99f5b34be0228c5e8a2cb224
Ronan Amicel <ronan.amicel@gmail.com>
2018-12-15 15:55:36 +0100

    Day 11, part 2: try some recursivity, still slow :(

--- 03-advent-of-code-2018/day-11/day-11.py
+++ 03-advent-of-code-2018/day-11/day-11.py
@@ -27,14 +27,23 @@ def cell_power_level(coords=None, serial_number=None):
     return hundreds_digit - 5
 
 
-@pytest.mark.parametrize("serial_number, min_size, max_size, coords, size, total", [
-    (18, 3, 3, (33, 45), 3, 29),
-    (42, 3, 3, (21, 61), 3, 30),
-    (18, 1, 300, (90, 269), 16, 113),
-    (42, 1, 300, (232, 251), 12, 119),
-])
-def test_square_with_largest_power_level(serial_number, min_size, max_size, coords, size, total):
-    assert square_with_largest_power_level(serial_number, min_size, max_size) == (total, coords, size)
+@pytest.mark.parametrize(
+    "serial_number, min_size, max_size, coords, size, total",
+    [
+        (18, 3, 3, (33, 45), 3, 29),
+        (42, 3, 3, (21, 61), 3, 30),
+        (18, 1, 300, (90, 269), 16, 113),
+        (42, 1, 300, (232, 251), 12, 119),
+    ],
+)
+def test_square_with_largest_power_level(
+    serial_number, min_size, max_size, coords, size, total
+):
+    assert square_with_largest_power_level(serial_number, min_size, max_size) == (
+        total,
+        coords,
+        size,
+    )
 
 
 def square_with_largest_power_level(serial_number, min_size=3, max_size=3):
@@ -46,15 +55,22 @@ def square_with_largest_power_level(serial_number, min_size=3, max_size=3):
     )
 
 
+@lru_cache(maxsize=None)
 def square_power_level(coords, serial_number, size):
+    if size == 1:
+        return cell_power_level(coords, serial_number)
     x, y = coords
-    return sum(
-        cell_power_level((x + dx, y + dy), serial_number)
-        for dx in range(size)
-        for dy in range(size)
+    x_right = x + size - 1
+    y_bottom = y + size - 1
+    return (
+        square_power_level(coords, serial_number, size - 1)
+        + sum(cell_power_level((x + dx, y_bottom), serial_number) for dx in range(size))
+        + sum(
+            cell_power_level((x_right, y + dy), serial_number) for dy in range(size - 1)
+        )
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(square_with_largest_power_level(8199))
     print(square_with_largest_power_level(8199, min_size=1, max_size=300))
