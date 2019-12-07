e2982d6c3ecfa8168178b4697881e114c644af78
Ronan Amicel <ronan.amicel@gmail.com>
2019-12-07 17:11:11 +0100

    Add day 4, part 2, of Advent of Code 2019 (Python)

--- 04-advent-of-code-2019/day04.py
+++ 04-advent-of-code-2019/day04.py
@@ -1,4 +1,5 @@
 import re
+from itertools import groupby
 
 import pytest
 
@@ -35,5 +36,28 @@ def part1(lower, upper):
     return sum(1 for n in range(lower, upper + 1) if matches(str(n)))
 
 
+@pytest.mark.parametrize("password", ["112233", "111122"])
+def test_matches2(password):
+    assert matches2(password)
+
+
+@pytest.mark.parametrize("password", ["123444"])
+def test_not_matches2(password):
+    assert not matches2(password)
+
+
+def matches2(password):
+    return has_exactly_two_adjacent_digits(password) and digits_never_decrease(password)
+
+
+def has_exactly_two_adjacent_digits(password):
+    return any(len(list(group)) == 2 for _, group in groupby(password))
+
+
+def part2(lower, upper):
+    return sum(1 for n in range(lower, upper + 1) if matches2(str(n)))
+
+
 if __name__ == "__main__":
     print("Part 1:", part1(152085, 670283))
+    print("Part 2:", part2(152085, 670283))
