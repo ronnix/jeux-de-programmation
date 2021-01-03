2dbf1db1e9dfd6b9036ea6a024f49d2975cb476f
Ronan Amicel <ronan.amicel@gmail.com>
2021-01-03 22:43:25 +0100

    AoC 2020, day 10, part 2 (Python)

--- 05-advent-of-code-2020/day10.py
+++ 05-advent-of-code-2020/day10.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from functools import lru_cache
 from textwrap import dedent
 
 from more_itertools import windowed
@@ -112,7 +113,66 @@ def part1(adapters):
     return n1 * n3
 
 
+def test_arrangements(sample_adapters):
+    assert arrangements(sample_adapters) == {
+        (0, 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22),
+        (0, 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, 22),
+        (0, 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, 22),
+        (0, 1, 4, 5, 7, 10, 12, 15, 16, 19, 22),
+        (0, 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, 22),
+        (0, 1, 4, 6, 7, 10, 12, 15, 16, 19, 22),
+        (0, 1, 4, 7, 10, 11, 12, 15, 16, 19, 22),
+        (0, 1, 4, 7, 10, 12, 15, 16, 19, 22),
+    }
+
+
+def test_larger_arrangements(larger_example):
+    assert len(arrangements(larger_example)) == 19208
+
+
+def arrangements(adapters):
+    last = device_joltage(adapters)
+
+    def _arrangements(sequence):
+        previous = sequence[-1]
+        for adapter in adapters:
+            if 1 <= adapter - previous <= 3:
+                yield from _arrangements(sequence + [adapter])
+        if 1 <= last - previous <= 3:
+            yield sequence + [last]
+
+    return {tuple(a) for a in _arrangements([0])}
+
+
+def test_efficiently_count_arrangements(sample_adapters):
+    assert efficiently_count_arrangements(sample_adapters) == 8
+
+
+def test_efficiently_count_larger_arrangements(larger_example):
+    assert efficiently_count_arrangements(larger_example) == 19208
+
+
+def efficiently_count_arrangements(adapters):
+    start = 0
+    end = device_joltage(adapters)
+    nodes = sorted(adapters | {start, end})
+
+    @lru_cache
+    def nb_paths(origin, destination):
+        if origin == destination:
+            return 1
+        successors = {node for node in nodes if 1 <= node - origin <= 3}
+        return sum(nb_paths(node, destination) for node in successors)
+
+    return nb_paths(start, end)
+
+
+def part2(adapters):
+    return efficiently_count_arrangements(adapters)
+
+
 if __name__ == "__main__":
     with open("day10.txt") as f:
         adapters = parse_input(f.read())
     print("Part 1:", part1(adapters))
+    print("Part 2:", part2(adapters))
