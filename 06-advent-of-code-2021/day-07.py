751e9e6002b60bf88d5f4c59ef8132c295879afc
Ronan Amicel <ronan.amicel@gmail.com>
2021-12-07 21:33:55 +0100

    AoC 2021, day 7, part 2 (Python)

--- 06-advent-of-code-2021/day-07.py
+++ 06-advent-of-code-2021/day-07.py
@@ -1,6 +1,6 @@
 # https://adventofcode.com/2021/day/7
 
-from typing import List
+from typing import Callable, List
 
 
 SAMPLE_INPUT = "16,1,2,0,4,2,7,1,2,14"
@@ -9,26 +9,40 @@ SAMPLE_INPUT = "16,1,2,0,4,2,7,1,2,14"
 # === Part 1 ===
 
 
-def test_parsing():
-    assert parse(SAMPLE_INPUT) == [16,1,2,0,4,2,7,1,2,14]
+def test_parsing() -> None:
+    assert parse(SAMPLE_INPUT) == [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
 
-def test_part1():
+def test_part1() -> None:
     assert part1(parse(SAMPLE_INPUT)) == 37
 
 
 def part1(xs: List[int]) -> int:
-    return min_required_fuel(xs)
+    return min_required_fuel(xs, lambda dx: dx)
 
 
-def min_required_fuel(xs: List[int]) -> int:
+FuelFunc = Callable[[int], int]
+
+
+def min_required_fuel(xs: List[int], func: FuelFunc) -> int:
     min_x = min(xs)
     max_x = max(xs)
-    return min(required_fuel(xs, x) for x in range(min_x, max_x + 1))
+    return min(required_fuel(xs, x, func) for x in range(min_x, max_x + 1))
 
 
-def required_fuel(xs: List[int], target_x: int):
-    return sum(abs(x - target_x) for x in xs)
+def required_fuel(xs: List[int], target_x: int, func: FuelFunc) -> int:
+    return sum(func(abs(x - target_x)) for x in xs)
+
+
+# === Part 2 ===
+
+
+def test_part2() -> None:
+    assert part2(parse(SAMPLE_INPUT)) == 168
+
+
+def part2(xs: List[int]) -> int:
+    return min_required_fuel(xs, lambda dx: (dx * (dx + 1)) // 2)
 
 
 # === Input parsing ===
@@ -46,3 +60,4 @@ def parse(text: str) -> List[int]:
 if __name__ == "__main__":
     xs = parse(read_input())
     print("Part 1:", part1(xs))
+    print("Part 2:", part2(xs))
