6187f0f27a36b9d040340a7cbeda2ae43895a4db
Ronan Amicel <ronan.amicel@gmail.com>
2021-12-08 22:55:26 +0100

    AoC 2021, day 8, part 2 (Python)

--- 06-advent-of-code-2021/day-08.py
+++ 06-advent-of-code-2021/day-08.py
@@ -1,7 +1,8 @@
 # https://adventofcode.com/2021/day/8
 
-from typing import FrozenSet, List, Set, Tuple
+from typing import Dict, FrozenSet, List, Set, Tuple
 
+from constraint import AllDifferentConstraint, InSetConstraint, Problem
 
 SHORT_INPUT = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
 
@@ -17,18 +18,20 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
 
-CORRECT_PATTERNS = {
-    0: set("abcefg"),   # 6 segments
-    1: set("cf"),       # 2 segments
-    2: set("acdeg"),    # 5 segments
-    3: set("acdfg"),    # 5 segments
-    4: set("bcdf"),     # 4 segments
-    5: set("abdfg"),    # 5 segments
-    6: set("abdefg"),   # 6 segments
-    7: set("acf"),      # 3 segments
-    8: set("abcdefg"),  # 7 segments
-    9: set("abcdfg"),   # 6 segments
+DIGIT_TO_SEGMENTS = {
+    0: frozenset("abcefg"),
+    1: frozenset("cf"),
+    2: frozenset("acdeg"),
+    3: frozenset("acdfg"),
+    4: frozenset("bcdf"),
+    5: frozenset("abdfg"),
+    6: frozenset("abdefg"),
+    7: frozenset("acf"),
+    8: frozenset("abcdefg"),
+    9: frozenset("abcdfg"),
 }
+
+SEGMENTS_TO_DIGIT = {v: k for k, v in DIGIT_TO_SEGMENTS.items()}
 
 Entry = Tuple[Set[FrozenSet[str]], List[FrozenSet[str]]]
 
@@ -62,11 +65,11 @@ def test_parsing() -> None:
     }
 
     assert output_value == [
-            frozenset("cdfeb"),
-            frozenset("fcadb"),
-            frozenset("cdfeb"),
-            frozenset("cdbaf"),
-        ]
+        frozenset("cdfeb"),
+        frozenset("fcadb"),
+        frozenset("cdfeb"),
+        frozenset("cdbaf"),
+    ]
 
 
 def test_part1() -> None:
@@ -84,16 +87,95 @@ def count_digits(entries: List[Entry], digits: Set[int]) -> int:
 def count_digit(entries: List[Entry], digit: int) -> int:
     count = 0
     for signal_patterns, output_value in entries:
-        segments = find_matching_segments_for(signal_patterns, digit)
+        segments = find_possible_signals_for(signal_patterns, digit)
         count += sum(1 for v in output_value if v == segments)
     return count
 
 
-def find_matching_segments_for(signal_patterns: Set[FrozenSet[str]], digit: int) -> FrozenSet[str]:
+def find_possible_signals_for(
+    signal_patterns: Set[FrozenSet[str]], digit: int
+) -> FrozenSet[str]:
     for pattern in signal_patterns:
-        if len(pattern) == len(CORRECT_PATTERNS[digit]):
+        if len(pattern) == len(DIGIT_TO_SEGMENTS[digit]):
             return pattern
     raise ValueError
+
+
+# === Part 2 ===
+
+
+def test_part2_short() -> None:
+    entry = parse_entry(SHORT_INPUT)
+    assert part2([entry]) == 5353
+
+
+def test_part2_longer() -> None:
+    entries = parse(LONGER_INPUT)
+    assert part2(entries) == 61229
+
+
+def part2(entries: List[Entry]) -> int:
+    return sum(solve_entry(entry) for entry in entries)
+
+
+def solve_entry(entry: Entry) -> int:
+    signal_patterns, output_value = entry
+    mapping = find_mapping(signal_patterns)
+    return int("".join(translate_signal(signal, mapping) for signal in output_value))
+
+
+def translate_signal(value: FrozenSet[str], mapping: Dict[str, str]) -> str:
+    segments = frozenset(mapping[signal] for signal in value)
+    return str(SEGMENTS_TO_DIGIT[segments])
+
+
+def test_find_mapping() -> None:
+    signal_patterns, _ = parse_entry(SHORT_INPUT)
+    mapping = find_mapping(signal_patterns)
+    assert mapping == {
+        "a": "c",
+        "b": "f",
+        "c": "g",
+        "d": "a",
+        "e": "b",
+        "f": "d",
+        "g": "e",
+    }
+
+
+def find_mapping(signal_patterns: Set[FrozenSet[str]]) -> Dict[str, str]:
+
+    # Let’s express this as a constraint satisfaction problem
+    problem = Problem()
+    for signal in "abcdefg":
+        problem.addVariable(signal, "abcdefg")
+
+    # Each signal wire goes to a different segment
+    problem.addConstraint(AllDifferentConstraint())
+
+    # Unambiguous digits based on count of lit segments
+    for digit in {1, 4, 7, 8}:
+        for wire in find_possible_signals_for(signal_patterns, digit):
+            segments = DIGIT_TO_SEGMENTS[digit]
+            problem.addConstraint(InSetConstraint(segments), wire)
+
+    # Unambiguous segments based on how many times they appear in patterns
+    for signal_wire in {"a", "b", "c", "d", "e", "f", "g"}:
+        count = sum(1 for pattern in signal_patterns if signal_wire in pattern)
+        if count == 4:
+            problem.addConstraint(InSetConstraint(["e"]), signal_wire)
+        elif count == 6:
+            problem.addConstraint(InSetConstraint(["b"]), signal_wire)
+        elif count == 7:
+            problem.addConstraint(InSetConstraint(["d", "g"]), signal_wire)
+        elif count == 8:
+            problem.addConstraint(InSetConstraint(["a", "c"]), signal_wire)
+        elif count == 9:
+            problem.addConstraint(InSetConstraint(["f"]), signal_wire)
+        else:
+            raise ValueError
+
+    return problem.getSolution()
 
 
 # === Input parsing ===
@@ -126,3 +208,4 @@ def parse_output_value(text: str) -> List[FrozenSet[str]]:
 if __name__ == "__main__":
     entries = parse(read_input())
     print("Part 1:", part1(entries))
+    print("Part 2:", part2(entries))
