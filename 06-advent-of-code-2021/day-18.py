223ae1e7ec592c7b843fc5585b25ed641527c2a9
Ronan Amicel <ronan.amicel@gmail.com>
2021-12-22 20:24:16 +0100

    AoC 2021, day 18, part 2 (Python)

--- 06-advent-of-code-2021/day-18.py
+++ 06-advent-of-code-2021/day-18.py
@@ -2,11 +2,24 @@
 
 from dataclasses import dataclass
 from enum import Enum
-from itertools import pairwise  # Python ≥ 3.10
+from itertools import permutations, pairwise  # Python ≥ 3.10
 from typing import Any, Iterator, List, Optional, Union, Tuple
 
 from more_itertools import peekable
 import pytest
+
+
+SAMPLE_INPUT = """\
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"""
 
 
 # === Part 1 ===
@@ -286,24 +299,28 @@ def test_magnitude(number: str, magnitude: int) -> None:
 
 
 def test_magnitude_of_sum() -> None:
-    numbers = parse(
-        """\
-[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-[[[5,[2,8]],4],[5,[[9,9],0]]]
-[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-[[[[5,4],[7,7]],8],[[8,3],8]]
-[[9,3],[[9,9],[6,[4,9]]]]
-[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"""
-    )
+    numbers = parse(SAMPLE_INPUT)
     assert snail_sum(numbers).magnitude() == 4140
 
 
 def part1(numbers: List[SnailFishNumber]) -> int:
     return snail_sum(numbers).magnitude()
+
+
+# === Part 2 ===
+
+
+def test_largest_magnitude_of_any_sum_of_two() -> None:
+    numbers = parse(SAMPLE_INPUT)
+    assert largest_magnitude_of_any_sum_of_two(numbers) == 3993
+
+
+def largest_magnitude_of_any_sum_of_two(numbers: List[SnailFishNumber]) -> int:
+    return max((a + b).magnitude() for a, b in permutations(numbers, 2))
+
+
+def part2(numbers: List[SnailFishNumber]) -> int:
+    return largest_magnitude_of_any_sum_of_two(numbers)
 
 
 # === Input parsing ===
@@ -321,3 +338,4 @@ def parse(text: str) -> List[SnailFishNumber]:
 if __name__ == "__main__":
     target = parse(read_input())
     print("Part 1:", part1(target))
+    print("Part 2:", part2(target))
