eab105530f8242c6db6536b2736d93a99ffde32e
Ronan Amicel <ronan.amicel@gmail.com>
2022-12-02 10:05:38 +0100

    We can just slice the result of sorted as it’s a list

--- 07-advent-of-code-2022/day-01.py
+++ 07-advent-of-code-2022/day-01.py
@@ -1,6 +1,6 @@
 # https://adventofcode.com/2022/day/1
 
-from more_itertools import islice_extended as islice, split_at
+from more_itertools import split_at
 
 
 EXAMPLE_INPUT = """\
@@ -33,7 +33,7 @@ def part1(text):
 
 
 def sum_n_largest(values, n):
-    return sum(islice(sorted(values))[-n:])
+    return sum(sorted(values)[-n:])
 
 
 def elf_calories(text):
