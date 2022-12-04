7297675efbbaf116f441f9987ebc2b1b914efdc3
Ronan Amicel <ronan.amicel@gmail.com>
2022-12-04 11:37:55 +0100

    AoC 2022, day 4, part 1 (Python)

---
+++ 07-advent-of-code-2022/day-04.py
@@ -0,0 +1,50 @@
+# https://adventofcode.com/2022/day/4
+
+from __future__ import annotations
+
+from more_itertools import quantify
+
+
+EXAMPLE_INPUT = """\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+"""
+
+
+# === Part 1 ===
+
+
+def test_part1():
+    assert part1(EXAMPLE_INPUT) == 2
+
+
+def part1(text: str) -> int:
+    pairs = [line.split(",") for line in text.splitlines()]
+    return quantify(pairs, completely_overlap)
+
+
+def completely_overlap(pair):
+    first, second = map(Range, pair)
+    return first.includes(second) or second.includes(first)
+
+
+class Range:
+    def __init__(self, s: str):
+        self.start, self.end = map(int, s.split("-"))
+
+    def includes(self, other: Range) -> bool:
+        return self.start <= other.start and self.end >= other.end
+
+
+def read_puzzle_input() -> str:
+    with open(__file__.removesuffix("py") + "txt") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    text = read_puzzle_input()
+    print("Part 1:", part1(text))
