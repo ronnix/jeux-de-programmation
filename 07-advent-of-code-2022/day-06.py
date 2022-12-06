f19d1204a31338ce0e6b0cd1f00df28e89a31649
Ronan Amicel <ronan.amicel@gmail.com>
2022-12-06 10:13:01 +0100

    AoC 2022, day 6, part 2 (Python)

--- 07-advent-of-code-2022/day-06.py
+++ 07-advent-of-code-2022/day-06.py
@@ -21,10 +21,35 @@ def test_part1(signal, result):
 
 
 def part1(text: str) -> int:
-    for index, chars in enumerate(windowed(text, 4)):
-        if len(set(chars)) == 4:
-            return index + 4
-    raise RuntimeError
+    return find_sequence_of_different_chars(text, 4)
+
+
+def find_sequence_of_different_chars(text: str, length: int) -> int:
+    for index, chars in enumerate(windowed(text, length)):
+        if len(set(chars)) == length:
+            return index + length
+    raise KeyError
+
+
+# === Part 2 ===
+
+
+@pytest.mark.parametrize(
+    "signal,result",
+    [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ],
+)
+def test_part2(signal, result):
+    assert part2(signal) == result
+
+
+def part2(text: str) -> int:
+    return find_sequence_of_different_chars(text, 14)
 
 
 def read_puzzle_input() -> str:
@@ -35,3 +60,4 @@ def read_puzzle_input() -> str:
 if __name__ == "__main__":
     text = read_puzzle_input()
     print("Part 1:", part1(text))
+    print("Part 2:", part2(text))
