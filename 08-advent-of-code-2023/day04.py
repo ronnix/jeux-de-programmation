0dad8de2c40948f29ba86d9aeb3e5688fd05a580
Ronan Amicel <ronan.amicel@gmail.com>
2023-12-06 09:32:37 +0100

    AoC 2023, day 4, part 2 (Python)

--- 08-advent-of-code-2023/day04.py
+++ 08-advent-of-code-2023/day04.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2023/day/4
 from __future__ import annotations
 
+from collections import Counter
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Set
@@ -30,6 +31,7 @@ def part1(text: str) -> int:
 
 def test_parse_card():
     card = Card.from_string("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+    assert card.number == 1
     assert card.winning_numbers == {41, 48, 83, 86, 17}
     assert card.numbers_you_have == {83, 86, 6, 31, 17, 9, 48, 53}
     assert card.points
@@ -40,8 +42,26 @@ def test_card_points():
     assert [card.points() for card in cards] == [8, 2, 2, 1, 0, 0]
 
 
+def test_part2():
+    assert part2(EXAMPLE_CARDS) == 30
+
+
+def part2(text: str) -> int:
+    cards = {
+        card.number: card
+        for card in (Card.from_string(line) for line in text.splitlines() if line)
+    }
+    counts = Counter()
+    for card in cards.values():
+        counts[card.number] += 1
+        for i in range(1, card.number_of_matching_numbers() + 1):
+            counts[card.number + i] += counts[card.number]
+    return counts.total()
+
+
 @dataclass
 class Card:
+    number: int
     winning_numbers: Set[int]
     numbers_you_have: Set[int]
 
@@ -50,19 +70,24 @@ class Card:
         identification, numbers = text.split(":")
         winning_numbers, numbers_you_have = numbers.split("|")
         return cls(
+            number=int(re.split(r"\s+", identification)[1]),
             winning_numbers={int(n) for n in re.split(r"\s+", winning_numbers.strip())},
             numbers_you_have={
                 int(n) for n in re.split(r"\s+", numbers_you_have.strip())
             },
         )
 
-    def points(self):
-        nb_winning = len(self.numbers_you_have.intersection(self.winning_numbers))
-        if nb_winning > 0:
-            return 2 ** (nb_winning - 1)
+    def points(self) -> int:
+        nb_matching = self.number_of_matching_numbers()
+        if nb_matching > 0:
+            return 2 ** (nb_matching - 1)
         return 0
+
+    def number_of_matching_numbers(self) -> int:
+        return len(self.numbers_you_have.intersection(self.winning_numbers))
 
 
 if __name__ == "__main__":
     puzzle_input = Path("day04.txt").read_text()
     print("Part 1", part1(puzzle_input))
+    print("Part 2", part2(puzzle_input))
