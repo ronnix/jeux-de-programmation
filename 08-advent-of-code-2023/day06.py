259c21d9135ea23626742093ac237791686b5bb3
Ronan Amicel <ronan.amicel@gmail.com>
2023-12-09 09:33:23 +0100

    AoC 2023, day 6, part 2 (Python)

--- 08-advent-of-code-2023/day06.py
+++ 08-advent-of-code-2023/day06.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from functools import reduce
+from math import ceil, floor, sqrt
 from pathlib import Path
 from typing import Iterable, List
 import operator
@@ -52,14 +53,23 @@ class Race:
         return [Race(time, distance) for time, distance in zip(times, distances)]
 
     def nb_ways_to_win(self) -> int:
-        res = 0
-        for charge_time in range(self.time + 1):
-            speed = charge_time
-            remaining_time = self.time - charge_time
-            distance = speed * remaining_time
-            if distance > self.distance:
-                res += 1
-        return res
+        # We solve the equation "x * (b - x) > C" => "b.x - x^2 > c" => "x^2 - b.x + c < 0"
+        # where a = 1,  b = -time and c = distance to find the two values of x where the
+        # distance is equal to the distance.
+
+        delta = (self.time**2) - 4 * self.distance  # delta = b^2 - 4.a.c
+        assert delta > 0
+        s1 = (self.time - sqrt(delta)) / 2
+        s2 = (self.time + sqrt(delta)) / 2
+
+        # Those are floating point numbers, now we want :
+        # - the smallest integer time value at which we start beat the record
+        # - the largest integer time value at which we still beat the record
+        smallest = floor(s1 + 1)
+        largest = ceil(s2 - 1)
+
+        # How many ways in this range?
+        return largest - smallest + 1
 
 
 def test_part1():
@@ -68,6 +78,15 @@ def test_part1():
 
 def part1(text: str) -> int:
     races = Race.parse(text)
+    return product(race.nb_ways_to_win() for race in races)
+
+
+def test_part2():
+    assert part2(EXAMPLE) == 71503
+
+
+def part2(text: str) -> int:
+    races = Race.parse(text.replace(" ", ""))
     return product(race.nb_ways_to_win() for race in races)
 
 
@@ -83,3 +102,4 @@ def read_puzzle_input():
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
     print("Part 1", part1(puzzle_input))
+    print("Part 2", part2(puzzle_input))
