f48822e2b81a2d79d427d0326f312932a9acedb1
Ronan Amicel <ronan.amicel@gmail.com>
2023-12-10 11:09:40 +0100

    AoC 2023, day 8, part 2 (Python)

--- 08-advent-of-code-2023/day08.py
+++ 08-advent-of-code-2023/day08.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import IntEnum
 from itertools import cycle
+from math import lcm
 from typing import Dict, List, Tuple
 import re
 
@@ -39,7 +40,7 @@ def test_parse_map():
 
 def test_path():
     m = Map.from_string(EXAMPLE_MAP)
-    assert m.path() == ["CCC", "ZZZ"]
+    assert m.path("AAA") == ["CCC", "ZZZ"]
 
 
 class Direction(IntEnum):
@@ -75,15 +76,15 @@ class Map:
 
     @classmethod
     def parse_node(cls, line: str) -> Tuple[Node, Tuple[Node, Node]]:
-        mo = re.match(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)", line)
+        mo = re.match(r"(\w{3}) = \((\w{3}), (\w{3})\)", line)
         assert mo is not None
         return (mo.group(1), (mo.group(2), mo.group(3)))
 
-    def path(self, origin: Node = "AAA", destination: Node = "ZZZ") -> List[Node]:
+    def path(self, origin: Node) -> List[Node]:
         node = origin
         result = []
         instructions = cycle(self.instructions)
-        while node != destination:
+        while not node.endswith("Z"):
             node = self.network[node][next(instructions)]
             result.append(node)
         return result
@@ -95,7 +96,31 @@ def test_part1():
 
 def part1(text: str) -> int:
     m = Map.from_string(text)
-    return len(m.path())
+    return len(m.path("AAA"))
+
+
+NEW_EXAMPLE_MAP = """\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+
+
+def test_part2():
+    assert part2(NEW_EXAMPLE_MAP) == 6
+
+
+def part2(text: str) -> int:
+    m = Map.from_string(text)
+    origins = [node for node in m.network if node.endswith("A")]
+    return lcm(*(len(m.path(origin)) for origin in origins))  # least common multiple
 
 
 def read_puzzle_input():
@@ -106,3 +131,4 @@ def read_puzzle_input():
 if __name__ == "__main__":
     puzzle_input = read_puzzle_input()
     print("Part 1", part1(puzzle_input))
+    print("Part 2", part2(puzzle_input))
